d71243b68e13cd6c214b193cc9e3800ccf9dece0
twititcoding <76032103+twititcoding@users.noreply.github.com>
2021-05-13 01:33:13 +0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+time.sleep(10)
+for i in range(200):
+    pyautogui.typewrite("Ayang vc ne diangkat  ")
+    pyautogui.press("enter")
+    pyautogui.typewrite("sampean dimna  ")
+    pyautogui.press("enter")
+    pyautogui.typewrite("yang diangkat vc ne")
+    pyautogui.press("enter")
+    pyautogui.typewrite("ayang kamu di mna")
+    pyautogui.press("enter")
